4360dc68df5a1f6d430db50d229092d9c86dd42e
hebusch <hwbusch@uc.cl>
2022-11-02 10:36:00 -0300

    feat: added home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ const Home = () => {
     return (
         <>
             <Navbar />
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-3xl font-bold">Tarea Development</h1>
+                <p className="text-center"> Desarrollado por Heinz Busch </p>
+            </div>
         </>
     )
 
