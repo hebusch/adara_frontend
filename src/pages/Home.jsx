cff0630c7c7a6b6aa3eca536ab9d4e30356e5fc7
hebusch <hwbusch@uc.cl>
2022-11-05 12:14:08 -0300

    feat: added registration and validations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import {React} from 'react'
+import { useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Registered from '../components/Registered';
 
 const Home = () => {
+    const location = useLocation();
 
     return (
         <>
             <Navbar />
-            <div className="flex flex-col items-center justify-center">
+            {location?.state?.registered && <Registered /> }
+            <div className="flex flex-col items-center justify-center mt-4">
                 <h1 className="text-3xl font-bold">Tarea Development</h1>
                 <p className="text-center"> Desarrollado por Heinz Busch </p>
             </div>
