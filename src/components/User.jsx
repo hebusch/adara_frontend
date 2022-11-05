4d0d9de9251f12ba3dbdd6581fe690046518e1ab
hebusch <hwbusch@uc.cl>
2022-11-05 13:35:30 -0300

    feat: added Users view

---
+++ src/components/User.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { HiEmojiSad } from 'react-icons/hi'
+
+
+const User = ({user}) => {
+
+    if(!user.name)
+    {
+        return(
+            <div className="flex flex-col gap-2 text-center mt-4 justify-center items-center">
+                <HiEmojiSad size={50} className="text-teal-600"/>
+                <p> No se encontró el usuario </p>
+            </div>
+        )
+    }
+
+
+
+    return (
+        <div className="flex flex-col gap-2 text-center mt-4">
+            <p> Nombre: {user.name} </p>
+            <p> Apellido: {user.lastName} </p>
+            <p> Email: {user.email} </p>
+            <p> Fecha de nacimiento: {user.birthday} </p>
+            <p> Género: {user.gender}</p>
+            <p> Región: {user.region}</p>
+            <p> Comuna: {user.comuna}</p>
+            <p> Talla: {user.shoe_size} (cm)</p>
+        </div>
+    )
+}
+
+export default User
